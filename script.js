e20eb96fbca0ee42636a9ce875edfe4bcf6bf19f
Alikhan <chort445@gmail.com>
2024-11-05 14:36:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//Tapsyrma 1)
+let age = 17;
+
+if (age > 16) {
+    console.log("Askan");
+}else{
+    console.log("Aspagan");
+}
+
+
+
+//Tapsyrma2)
+let num  = 9;
+
+if (num % 2) {
+    console.log("Taq san");
+}else{
+    console.log("Jup san");
+}
+
+
+
+//Tapsyrma3)
+let temperature = 22;
+
+console.log(temperature > 20 && temperature < 30);
+
+
+//Tapsyrma4)
+let number = 5; 
+if (number > 0) {
+    console.log("On");
+} else {
+    console.log("On emes");
+}
+
+
+
+//Tapsyrma5)
+let b = 50; 
+if (b % 5 === 0 && b % 10 === 0) {
+    console.log("true");
+} else {
+    console.log("false");
+}
